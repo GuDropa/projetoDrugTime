59d33f6c5204050386308d4852978c6de6c6a451
Lucas <lucas@webfloat.com.br>
2021-10-24 23:38:10 -0300

    japobala

---
+++ teste/lib/data/dummy_remedios.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:teste/models/remedio.dart';
+
+const DUMMY_REMEDIOS = {
+  '1' : const Remedio(
+    id: '1',
+    name: 'Dramin',
+    hour: TimeOfDay(hour:0,minute:0),
+  ),
+  '2' : const Remedio(
+    id: '2',
+    name: 'Sla',
+    hour: TimeOfDay(hour:0,minute:0),
+  ),
+};
